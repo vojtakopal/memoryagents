2991bada8a6ae59d16e98f43048f4a4cca29322d
gufy <vojtech.kopal@allodium.eu>
2011-05-11 20:54:32 +0200

    texty, hratky s texem, obrazky

---
+++ docs/tex/build.bat
@@ -0,0 +1,11 @@
+latex bc_thesis.tex
+
+bibtex bc_thesis
+
+latex bc_thesis.tex
+
+latex bc_thesis.tex
+
+dvips bc_thesis.dvi
+
+ps2pdf bc_thesis.ps
---
+++ src/memagents/agents/QuadAgent.java
@@ -0,0 +1,11 @@
+package memagents.agents;
+
+import memagents.Simulation;
+
+public class QuadAgent extends RandomAgent {
+
+	public QuadAgent(Simulation simulation) {
+		super(simulation);
+	}
+
+}
